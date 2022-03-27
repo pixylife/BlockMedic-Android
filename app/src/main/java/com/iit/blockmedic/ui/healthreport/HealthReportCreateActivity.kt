c442bca34138d2a7dbd6a5a186fadc22a7a41a02
Sahan Thinusha <sahan@virtuan.io>
2022-03-27 10:06:13 +0530

    Ui

---
+++ app/src/main/java/com/iit/blockmedic/ui/healthreport/HealthReportCreateActivity.kt
@@ -0,0 +1,12 @@
+package com.iit.blockmedic.ui.healthreport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iit.blockmedic.R
+
+class HealthReportCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health_report_create)
+    }
+}
---
+++ app/src/main/java/com/iit/blockmedic/ui/patient/PatientViewListActivity.kt
@@ -0,0 +1,12 @@
+package com.iit.blockmedic.ui.patient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iit.blockmedic.R
+
+class PatientViewListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_view_list)
+    }
+}
